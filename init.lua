29bdcf2e6778595c8fb0d59adfb73ee9f0927197
Edgar Garcia Cano <eegkno@gmail.com>
2023-09-01 15:13:55 -0400

    Initial cofiguration

---
+++ init.lua
@@ -0,0 +1,6 @@
+-- References:
+-- https://vonheikemen.github.io/devlog/tools/build-your-first-lua-config-for-neovim/
+-- https://github.com/ThePrimeagen/init.lua
+-- https://github.com/ThePrimeagen/init.lua
+require("user")
+print("Welcome back!")
