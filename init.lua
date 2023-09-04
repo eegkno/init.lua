d7678db58277aa13e1f76fd577be16251e0e5b0c
Edgar Garcia Cano <eegkno@gmail.com>
2023-09-04 17:21:05 -0400

    Add lsp configoration

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@
 -- https://github.com/ThePrimeagen/init.lua
 -- https://github.com/cpow/cpow-dotfiles
 -- https://github.com/josean-dev/dev-environment-files
--- https://github.com/nvim-lua/kickstart.nvim:E
+-- https://github.com/nvim-lua/kickstart.nvim
+-- https://github.com/dreamsofcode-io/neovim-python
 require("user")
 print("Welcome back!")
